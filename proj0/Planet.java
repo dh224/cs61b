604b7cac623a70181b82f72c0dab45316bc62d70
duhang <583656269@qq.com>
2021-07-25 22:50:09 +0800

    fix some mistakes

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -5,7 +5,7 @@ public class Planet {
     public double yyVel;
     public double mass;
     public String imgFileName;
-    public static final double G = 6.67E-11;
+    private static final double G = 6.67E-11;
     public Planet(double xP,double yP, double xV,double yV,
                   double mass,String img){
         this.xxPos = xP;
