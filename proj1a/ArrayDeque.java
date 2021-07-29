de9bc3df3f8d889b7c4c9843652ddf10776aa20b
duhang <583656269@qq.com>
2021-07-29 19:39:46 +0800

    completed the proj1

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,113 @@
+public class ArrayDeque<T>{
+    private T[] items;
+    private int size;
+    private int first;
+    private int last;
+    private int cap;
+    public ArrayDeque(){
+        cap = 8;
+        first = cap - 1;
+        last = 0;
+        size = 0;
+        items = (T[]) new Object[cap];
+    }
+    private void resize(int capacity) {
+        T[] a = (T[]) new Object[capacity];
+        int f = first;
+        int t = size;
+        int i = 0;
+        f++;
+        if(f>=cap){
+            f=0;
+        }
+        while(t-- >0){
+            if(f >= cap) f = 0;
+            a[i++] = items[f++];
+        }
+        last = size;
+        first = cap -1;
+        items = a;
+    }
+    public ArrayDeque(ArrayDeque other){
+        this.cap = 8;
+        this.first = cap - 1;
+        this.last =0;
+        this.size =0;
+        this.items = (T[]) new Object[cap];
+        int t = other.size;
+        int i = 0;
+        while(t-- >0){
+            this.addLast((T)other.get(i++));
+        }
+    }
+    public boolean isFull(){
+        return (size == cap);
+    }
+    public void addFirst(T item){
+        if(isFull()){
+            resize(cap * 2);
+            cap *= 2;
+        }
+        items[first--] = item;
+        if(first < 0){
+            first = cap -1;
+        }
+        size++;
+    }
+    public void addLast(T item){
+        if(isFull()){
+            resize(cap * 2);
+            cap *= 2;
+        }
+        items[last++] = item;
+        if(last == cap) last = 0;
+        size++;
+    }
+    public boolean isEmpty(){
+        return size == 0;
+    }
+    public void printDeque(){
+        int f = first;
+        int t = size;
+        f++;
+        if(f>=cap){
+            f=0;
+        }
+        while(t-- >0){
+            if(f >= cap) f = 0;
+            System.out.print(items[f++] + " ");
+        }
+        System.out.println();
+    }
+    public T removeFirst(){
+        first++;
+        if(first >= cap ) first =0;
+        T t = items[first];
+        items[first] = null;
+        size --;
+        if(size <= (cap * 0.25 ) && cap >= 16){
+            resize((int)cap/2);
+            cap = (int)(cap /2);
+        }
+        return t;
+    }
+    public T removeLast(){
+        last++;
+        if(last >= cap) last =0;
+        T t = items[last];
+        items[last] = null;
+        size--;
+        if(size <= cap * 0.25 && cap >= 16){
+            resize((int)cap/2);
+            cap = (int)(cap/2);
+        }
+        return t;
+    }
+    public T get(int index){
+        int t = index + first;
+        if(t >= cap){
+            t %= cap;
+        }
+        return items[t];
+    }
+}
---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,103 @@
+public class LinkedListDeque<T> {
+    public class LinkNode {
+        public T item;
+        public LinkNode next;
+        public LinkNode prev;
+        public LinkNode(T item, LinkNode next,LinkNode prev){
+            this.item = item;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+    private LinkNode sentFirst;
+    private LinkNode sentLast;
+    private LinkNode last;
+    private int size;
+
+    public LinkedListDeque(){
+        sentFirst =  new LinkNode(null,null,null);
+        sentLast = new LinkNode(null,null,null);
+        sentFirst.next = sentLast;
+        sentLast.prev = sentFirst;
+        size = 0;
+    }
+    public LinkedListDeque(LinkedListDeque other){
+        sentFirst =  new LinkNode(null,null,null);
+        sentLast = new LinkNode(null,null,null);
+        sentFirst.next = sentLast;
+        sentLast.prev = sentFirst;
+        size = 0;
+        LinkNode p = other.sentFirst;
+        while(p.next != other.sentLast){
+            addLast(p.next.item);
+            p = p.next;
+        }
+    }
+    public void addFirst(T item){
+        LinkNode temp = new LinkNode(item,sentFirst.next,sentFirst);
+        sentFirst.next.prev = temp;
+        sentFirst.next = temp;
+        size++;
+    }
+    public void addLast(T item){
+        LinkNode temp = new LinkNode(item,sentLast,sentLast.prev);
+        sentLast.prev.next = temp;
+        sentLast.prev = temp;
+        size++;
+    }
+    public boolean isEmpty(){
+        return size == 0;
+    }
+    public int size(){
+        return size;
+    }
+    public void printDeque(){
+        LinkNode p = sentFirst;
+        while(p.next != sentLast){
+            System.out.print(p.next.item + " ");
+            p = p.next;
+        }
+        System.out.println();
+    }
+    public T removeFirst(){
+        if(sentFirst.next == sentLast) return null;
+        LinkNode  tN = sentFirst.next;
+        T t = tN.item;
+        sentFirst.next = tN.next;
+        tN.next.prev = sentFirst;
+        tN.item = null;
+        size--;
+        return t;
+    }
+    public T removeLast(){
+        if(sentFirst.next == sentLast) return null;
+        LinkNode tN = sentLast.prev;
+        T t = tN.item;
+        sentLast.prev = tN.prev;
+        tN.prev.next = sentLast;
+        tN.item = null;
+        size--;
+        return t;
+    }
+    public T get(int index){
+        if (index >= size) return null;
+        LinkNode p = sentFirst;
+        while(index-- >= 0){
+            p = p.next;
+        }
+        return p.item;
+    }
+    public T getRecursive(LinkNode p, int index){
+        if(index == 0){
+            return p.item;
+        }
+        return getRecursive(p.next,index -1);
+    }
+    public T getRecursive(int index){
+        LinkNode p = sentFirst;
+        if(p.next == sentLast) return null;
+        else{
+            return getRecursive(p.next,index);
+        }
+    }
+}
